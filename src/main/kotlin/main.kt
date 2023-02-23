8c37360e8261a41849692ea6555d22887fe5f836
Pavel Anorov <pavelanorov@yandex.ru>
2023-02-23 15:15:54 +0300

    ver 1.1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,31 +1,55 @@
 fun main() {
-    calculatePayment(transferAmount = 10000) // OK
-    calculatePayment(previousTransfers = 30000, transferAmount = 1000) // OK
-    calculatePayment(transferAmount = 20000) // Ошибка: максимальная сумма перевода VK Pay - 15000 рублей
-    calculatePayment(previousTransfers = 39000, transferAmount = 2000) // Ошибка: максимальная сумма переводов VK Pay в месяц - 40000 рублей
-}
+    // Константы для лимитов по картам
+    val dailyLimit = 150000
+    val monthlyLimit = 600000
+    // Константы для лимитов по VK Pay
+    val vkPayLimit = 15000
+    val vkPayMonthlyLimit = 40000
 
-fun calculatePayment(type: String = "VK Pay", previousTransfers: Int = 0, transferAmount: Int) {
-    val maxTransferAmount = 15000 // максимальная сумма перевода за раз
-    val maxMonthlyTransferAmount = 40000 // максимальная сумма переводов в месяц
-    val remainingMonthlyTransferAmount = maxMonthlyTransferAmount - previousTransfers // сколько еще можно перевести в этом месяце
-
-    if (type == "VK Pay") {
-        if (transferAmount > maxTransferAmount) {
-            println("Ошибка, нельзя столько переводить за 1 раз -  максимальная сумма перевода VK Pay - $maxTransferAmount рублей")
-            return
+    // Функция расчета комиссии для переводов с карт Mastercard и Maestro
+    fun calculateMastercardMaestroCommission(amount: Int, monthlyTransferSum: Int): Double {
+        return if (monthlyTransferSum <= monthlyLimit && amount <= dailyLimit) {
+            0.0
+        } else {
+            0.006 * amount + 20
         }
-
-        if (previousTransfers + transferAmount > maxMonthlyTransferAmount) {
-            println("Ошибка, вы исчерпали лимит в месяц переводов - максимальная сумма переводов VK Pay в месяц - $maxMonthlyTransferAmount рублей")
-            return
-        }
-        println("***********")
-        println("Тип карты: $type")
-        println("Сумма предыдущих переводов в этом месяце: $previousTransfers рублей")
-        println("Сумма совершенного перевода: $transferAmount рублей")
-        println("Осталось доступно для перевода в этом месяце: $remainingMonthlyTransferAmount рублей")
-    } else {
-        println("Тип карты $type не поддерживается")
     }
+
+    // Функция расчета комиссии для переводов с карт Visa и Mir
+    fun calculateVisaMirCommission(amount: Int): Double {
+        val commission = amount * 0.0075
+        return if (commission < 35) {
+            35.0
+        } else {
+            commission
+        }
+    }
+
+    // Функция проверки лимитов для переводов на VK Pay
+    fun checkVkPayLimits(amount: Int, monthlyTransferSum: Int): Boolean {
+        return amount <= vkPayLimit && monthlyTransferSum <= vkPayMonthlyLimit
+    }
+
+    // Пример использования функций
+    val amount = 17800
+    val cardType = "VK Pay"
+    val monthlyTransferSum = 0
+
+    val commission = when (cardType) {
+        "Mastercard", "Maestro" -> calculateMastercardMaestroCommission(amount, monthlyTransferSum)
+        "Visa", "Mir" -> calculateVisaMirCommission(amount)
+        "VK Pay" -> 0.0
+        else -> error("Неизвестный тип карты")
+    }
+
+    val total = amount + commission
+
+    if (cardType == "VK Pay" && checkVkPayLimits(amount, vkPayMonthlyLimit)) {
+        println("Перевод на VK Pay прошел успешно: $amount рублей.")
+    } else if (total <= dailyLimit && monthlyTransferSum <= monthlyLimit) {
+        println("Перевод с карты $cardType на сумму $amount рублей прошел успешно.")
+    } else {
+        println("Превышен лимит: сумма перевода в день не должна превышать $dailyLimit рублей, а сумма перевода в месяц не должна превышать $monthlyLimit рублей.")
+    }
+
 }
